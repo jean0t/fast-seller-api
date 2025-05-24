ee65a44955a6d73b749dfe6cdd685e220c9e58df
jean0t <example@example.com>
2025-05-24 14:21:17 -0300

    feat: categories added

--- app/app.py
+++ app/app.py
@@ -13,16 +13,16 @@ class App:
         self.register_routes()
 
     def clients_routes(self):
-        self.app.include_router(clients.router)
+        self.app.include_router(clients.router, prefix="/clients", tags=["Clients"])
 
     def products_routes(self):
-        self.app.include_router(products.router)
+        self.app.include_router(products.router, prefix="/products", tags=["Products"])
 
     def orders_routes(self):
-        self.app.include_router(orders.router)
+        self.app.include_router(orders.router, prefix="/orders", tags=["Orders"])
 
     def auth_routes(self):
-        self.app.include_router(auth.router)
+        self.app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 
     def register_routes(self):
         self.clients_routes()
